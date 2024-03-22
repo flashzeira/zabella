384d917350775fda0c9cbc6e0180153e597ebfb6
flashzeira <162184589+flashzeira@users.noreply.github.com>
2024-03-22 11:37:51 -0300

    heyheyhey

---
+++ index.js
@@ -0,0 +1,9 @@
+function eventoClick(){
+    alert('então vamos lá, passe o mouse no espaço em branco, após apertar o botao de ok aqui em baixo e não esqueça de voltar para essa pagina para avaliar depois');
+    document.body.style.backgroundColor = "black";
+}
+
+function eventoClick1(){
+    alert('se prepare então minha filha');
+    
+}
